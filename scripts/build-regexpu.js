a51c06c523c55be4aeb147ad60c1532403dc611d
Barak Igal <baraki@wix.com>
2020-05-28 02:10:17 +0300

    Add build to avoid using regexpu-core in runtime

---
+++ scripts/build-regexpu.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+var path = require("path");
+var regexpu = require("regexpu-core");
+
+var uniReg = {
+  typeMatchClass: regexpu(
+    "\\.((?:\\\\.|[A-Za-z_\\-\\u{00a0}-\\u{10ffff}])(?:\\\\.|[A-Za-z_\\-0-9\\u{00a0}-\\u{10ffff}])*)",
+    "u"
+  ),
+  typeMatchId: regexpu(
+    "#((?:\\\\.|[A-Za-z_\\-\\u{00a0}-\\u{10ffff}])(?:\\\\.|[A-Za-z_\\-0-9\\u{00a0}-\\u{10ffff}])*)",
+    "u"
+  ),
+  identifierEscapeRegexp: regexpu(
+    "(^[^A-Za-z_\\-\\u{00a0}-\\u{10ffff}]|^--|[^A-Za-z_0-9\\-\\u{00a0}-\\u{10ffff}])",
+    "ug"
+  ),
+};
+
+var targetFile = path.join(__dirname, "../lib/uni-regexp.js");
+
+fs.writeFileSync(
+  targetFile,
+  "/* AUTO GENERATED */\nmodule.exports = " + JSON.stringify(uniReg, null, 4)
+);
+console.log('Done building ' + targetFile)
